da66347cbdd8181aa2a3cc0302e50f9b7d0457e2
jjrmiya <jjrmiyamoto@gmail.com>
2021-02-11 18:37:40 +0900

    2章途中

---
+++ 2.js
@@ -0,0 +1,18 @@
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof true);
+console.log(typeof 42);
+console.log(typeof 42n);
+console.log(typeof "物質");
+console.log(typeof Symbol("貧しき漁夫"));
+console.log(typeof Math);
+
+//2.3
+const pi = 3.14;
+console.log((1*2)*pi);
+console.log(1*1*pi);
+const 記号 = "hoge";
+//const default = "hoge";
+let radius;
+console.log(radius);
+console.log(!true);
